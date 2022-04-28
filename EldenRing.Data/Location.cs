24ef0587b4d633f3c14f651a2d2c33a2503e0d4e
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-28 14:23:32 -0400

    added location class

--- EldenRing.Data/Location.cs
+++ EldenRing.Data/Location.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace EldenRing.Data
 {
     public class Location
     {
+        [Key]
+        public int LocationId { get; set; }
+        public string Name { get; set; }
+        public string Region { get; set; }
+        public List<Weapon> Weapons { get; set; } = new List<Weapon>();
+        public List<ArmorSet> ArmorSets { get; set; } = new List<ArmorSet>();
     }
 }
