b6cc588df906542aa01da5a7358f1c4600d9fbee
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-28 14:59:29 -0400

    WeaponDetail added

--- EldenRing.Models/WeaponModels/WeaponDetail.cs
+++ EldenRing.Models/WeaponModels/WeaponDetail.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EldenRing.Data;
 
 namespace EldenRing.Models.WeaponModels
 {
     public class WeaponDetail
     {
+        public string Name { get; set; }
+        public WeaponType TypeOfWeapon { get; set; }
+        public string StrengthScaling { get; set; }
+        public string DexterityScaling { get; set; }
+        public string IntelligenceScaling { get; set; }
+        public string FaithScaling { get; set; }
+        public string ArcaneScaling { get; set; }
+        public string PhysicalDamage { get; set; }
+        public string MagicDamage { get; set; }
+        public string FireDamage { get; set; }
+        public string LightDamage { get; set; }
+        public string HolyDamage { get; set; }
+        public bool Bleed { get; set; }
+        public bool Poison { get; set; }
+        public bool FrostBite { get; set; }
+        public bool ScarletRot { get; set; }
+        public bool Sleep { get; set; }
+        public bool Madness { get; set; }
+        public int LocationId { get; set; }
     }
 }
