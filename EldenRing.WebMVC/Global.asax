ca43a2a8c0a9d40eeec2090d57f99cd74b3a050d
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-28 13:38:02 -0400

    First Commit

---
+++ EldenRing.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EldenRing.WebMVC.MvcApplication" Language="C#" %>
---
+++ EldenRing.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EldenRing.WebMVC.Startup))]
+namespace EldenRing.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
