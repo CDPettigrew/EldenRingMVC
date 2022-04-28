eb9e36d1961e019ba29204bd5ef02d6ef6ede9a9
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-28 13:52:35 -0400

    Data classes made

---
+++ EldenRing.Data/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldenRing.Data
+{
+    public enum WeaponType { Dagger, StraighSword, GreatSword, ColossalSword, }
+    public class Weapon
+    {
+        [Key]
+        public int WeaponId { get; set; }
+        public string Name { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
